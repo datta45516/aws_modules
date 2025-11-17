87d5260db6d63dc2a3dbf570debabd39c3b93c48
datta45516 <sdattatray516@gmail.com>
2025-11-17 19:22:09 +0530

    Add AWS DynamoDB table resource configuration

---
+++ modules/dynamodb_table/main.tf
@@ -0,0 +1,12 @@
+resource "aws_dynamodb_table" "this" {
+  name         = var.name
+  hash_key     = var.hash_key
+  billing_mode = var.billing_mode
+
+  attribute {
+    name = var.hash_key
+    type = "S"
+  }
+
+  tags = var.tags
+}
