b74740b712cd4c32db6f546cddc462cd1a7a3ed1
datta45516 <sdattatray516@gmail.com>
2025-12-08 12:32:09 +0530

    Create variable.tf

---
+++ modules/eks/variable.tf
@@ -0,0 +1,6 @@
+
+variable "region" { default = "us-east-1" }
+variable "cluster_name" { type = string }
+variable "kubernetes_version" { default = "1.30" }
+variable "subnet_ids" { type = list(string) }
+variable "tags" { type = map(string) default = {} }
