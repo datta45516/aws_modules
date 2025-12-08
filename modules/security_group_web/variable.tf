351f07e51c783b661e3ce437009724d233edc9b7
datta45516 <sdattatray516@gmail.com>
2025-12-08 12:51:37 +0530

    Update variable.tf

--- modules/security_group_web/variable.tf
+++ modules/security_group_web/variable.tf
@@ -1,12 +1,7 @@
-variable "vpc_id" {
-  type = string
-}
 
-variable "allowed_http_cidrs" {
-  type = list(string)
-}
-
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
+variable "region" { default = "us-east-1" }
+variable "name" { type = string }
+variable "description" { type = string default = "Managed by Terraform" }
+variable "vpc_id" { type = string }
+variable "ingress_rules" { type = list(object({ cidr = string, from_port = number, to_port = number, protocol = string, description = string })) }
+variable "tags" { type = map(string) default = {} }
