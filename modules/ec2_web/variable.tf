78af59d4072b8a8c2c2ed4351a0bd08a8548db7e
datta45516 <sdattatray516@gmail.com>
2025-12-08 13:12:51 +0530

    Update variable.tf

--- modules/ec2_web/variable.tf
+++ modules/ec2_web/variable.tf
@@ -1,28 +1,10 @@
-variable "subnet_id" {
-  type = string
-}
 
-variable "security_group_ids" {
-  type = list(string)
-}
-
-variable "instance_type" {
-  type = string
-}
-
-variable "ami_id" {
-  type = string
-}
-
-variable "key_name" {
-  type = string
-}
-
-variable "iam_instance_profile_name" {
-  type = string
-}
-
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
+variable "region" { default = "us-east-1" }
+variable "name" { type = string }
+variable "ami_id" { type = string }
+variable "instance_type" { default = "t3.micro" }
+variable "subnet_id" { type = string }
+variable "security_group_ids" { type = list(string) }
+variable "key_name" { type = string default = "" }
+variable "user_data" { type = string default = "" }
+variable "tags" { type = map(string) default = {} }
