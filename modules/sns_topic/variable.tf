55267be627584145b454e84bb0d1df63a41cc580
datta45516 <sdattatray516@gmail.com>
2025-12-08 14:11:57 +0530

    Update variable.tf

--- modules/sns_topic/variable.tf
+++ modules/sns_topic/variable.tf
@@ -1,8 +1,5 @@
-variable "name" {
-  type = string
-}
 
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
+variable "region" { default = "us-east-1" }
+variable "topic_name" { type = string }
+variable "subscribers" { type = list(object({ protocol = string, endpoint = string })) default = [] }
+variable "tags" { type = map(string) default = {} }
