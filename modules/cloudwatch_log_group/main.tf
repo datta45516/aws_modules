986c84a9c7b18fc2000ac94c16f03dc3e353334a
datta45516 <sdattatray516@gmail.com>
2025-12-08 14:20:19 +0530

    Update main.tf

--- modules/cloudwatch_log_group/main.tf
+++ modules/cloudwatch_log_group/main.tf
@@ -1,5 +1,9 @@
+
+terraform { required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } } }
+provider "aws" { region = var.region }
+
 resource "aws_cloudwatch_log_group" "this" {
-  name              = var.name
-  retention_in_days = var.retention_in_days
+  name              = var.log_group_name
+  retention_in_days = var.retention_days
   tags              = var.tags
 }
