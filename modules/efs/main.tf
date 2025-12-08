abb7828652d5c7ae1c1fdd4191758f53eb9a5ab2
datta45516 <sdattatray516@gmail.com>
2025-12-08 14:38:47 +0530

    Create main.tf

---
+++ modules/efs/main.tf
@@ -0,0 +1,15 @@
+
+terraform { required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } } }
+provider "aws" { region = var.region }
+
+resource "aws_efs_file_system" "this" {
+  creation_token = var.name
+  tags           = var.tags
+}
+
+resource "aws_efs_mount_target" "this" {
+  count           = length(var.subnet_ids)
+  file_system_id  = aws_efs_file_system.this.id
+  subnet_id       = var.subnet_ids[count.index]
+  security_groups = var.security_group_ids
+}
