2b91f4fccfe6cfaf5eeaa316b30af29f218bf5f0
datta45516 <sdattatray516@gmail.com>
2025-12-08 13:22:14 +0530

    Update main.tf

--- modules/dynamodb_table/main.tf
+++ modules/dynamodb_table/main.tf
@@ -1,12 +1,22 @@
-resource "aws_dynamodb_table" "this" {
-  name         = var.name
-  hash_key     = var.hash_key
-  billing_mode = var.billing_mode
 
+terraform { required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } } }
+provider "aws" { region = var.region }
+
+resource "aws_dynamodb_table" "this" {
+  name           = var.table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = var.hash_key
+  range_key      = var.range_key
   attribute {
     name = var.hash_key
     type = "S"
   }
-
+  dynamic "attribute" {
+    for_each = var.range_key != "" ? [1] : []
+    content {
+      name = var.range_key
+      type = "S"
+    }
+  }
   tags = var.tags
 }
