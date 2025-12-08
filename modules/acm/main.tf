b17c353936d9ba8341bad478c9749c9603d07b3a
datta45516 <sdattatray516@gmail.com>
2025-12-08 14:17:25 +0530

    Create main.tf

---
+++ modules/acm/main.tf
@@ -0,0 +1,9 @@
+
+terraform { required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } } }
+provider "aws" { region = "us-east-1" }
+
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+  tags              = var.tags
+}
