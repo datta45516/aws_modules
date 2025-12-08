e713bab585ac70b684fe8c0966d9075eded18b0e
datta45516 <sdattatray516@gmail.com>
2025-12-08 13:12:11 +0530

    Update main.tf

--- modules/ec2_web/main.tf
+++ modules/ec2_web/main.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "this" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = var.security_group_ids
-  key_name                    = var.key_name
-  iam_instance_profile        = var.iam_instance_profile_name
-  associate_public_ip_address = true
 
-  tags = merge(var.tags, {
-    Name = "web-server"
-  })
+terraform { required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } } }
+provider "aws" { region = var.region }
+
+resource "aws_instance" "this" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  key_name               = var.key_name
+  user_data              = var.user_data
+  tags                   = merge(var.tags, { Name = var.name })
 }
