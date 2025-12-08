9300c7ebaa93637344c1e632ac8bfca7d65a6465
datta45516 <sdattatray516@gmail.com>
2025-12-08 14:11:18 +0530

    Update main.tf

--- modules/sns_topic/main.tf
+++ modules/sns_topic/main.tf
@@ -1,4 +1,15 @@
+
+terraform { required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } } }
+provider "aws" { region = var.region }
+
 resource "aws_sns_topic" "this" {
-  name = var.name
+  name = var.topic_name
   tags = var.tags
+}
+
+resource "aws_sns_topic_subscription" "this" {
+  count     = length(var.subscribers)
+  topic_arn = aws_sns_topic.this.arn
+  protocol  = var.subscribers[count.index].protocol
+  endpoint  = var.subscribers[count.index].endpoint
 }
