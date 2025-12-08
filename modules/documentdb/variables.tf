9a574222b4858bf1bfc1940a4219c7ff54ea514e
datta45516 <sdattatray516@gmail.com>
2025-12-08 14:59:25 +0530

    Create variables.tf

---
+++ modules/documentdb/variables.tf
@@ -0,0 +1,6 @@
+
+variable "region" { default = "us-east-1" }
+variable "name" { type = string }
+variable "username" { type = string }
+variable "password" { type = string sensitive = true }
+variable "subnet_ids" { type = list(string) }
