df58c5314aef7031dfbcec76288dbd5ee27d3e2b
datta45516 <sdattatray516@gmail.com>
2025-12-08 14:20:56 +0530

    Update variable.tf

--- modules/cloudwatch_log_group/variable.tf
+++ modules/cloudwatch_log_group/variable.tf
@@ -1,12 +1,5 @@
-variable "name" {
-  type = string
-}
 
-variable "retention_in_days" {
-  type = number
-}
-
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
+variable "region" { default = "us-east-1" }
+variable "log_group_name" { type = string }
+variable "retention_days" { default = 30 }
+variable "tags" { type = map(string) default = {} }
