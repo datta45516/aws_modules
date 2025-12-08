1d6514c00f1f691efb59993f993a7fcd16e7ab7b
datta45516 <sdattatray516@gmail.com>
2025-12-08 12:30:59 +0530

    Create main.tf

---
+++ modules/eks/main.tf
@@ -0,0 +1,34 @@
+
+terraform { required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } } }
+provider "aws" { region = var.region }
+
+resource "aws_iam_role" "cluster" {
+  name = "${var.cluster_name}-cluster-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal { Service = "eks.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  ])
+  role       = aws_iam_role.cluster.name
+  policy_arn = each.value
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.cluster.arn
+  version  = var.kubernetes_version
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+  tags = var.tags
+}
