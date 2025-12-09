556d962a0e0c4bae1e2aac70f302560fae10eaf0
datta45516 <sdattatray516@gmail.com>
2025-12-09 14:56:59 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,6 +1,8 @@
-
 variable "region" { default = "us-east-1" }
 variable "cluster_name" { type = string }
 variable "kubernetes_version" { default = "1.30" }
 variable "subnet_ids" { type = list(string) }
-variable "tags" { type = map(string) default = {} }
+variable "tags" {
+type = map(string)
+default = {}
+}
