02a05e5de6a153eb39eb3f8e156cf21fd08593a3
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:13:27 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,16 +1,9 @@
-
-variable "region" { default = "us-east-1" }
 variable "name" { type = string }
-variable "engine_version" { default = "16.0" }
-variable "instance_class" { default = "db.t3.micro" }
-variable "allocated_storage" { default = 20 }
 variable "username" { type = string }
-variable "password" {
-type = string
-sensitive = true
-}
+variable "password" { type = string sensitive = true }
 variable "subnet_ids" { type = list(string) }
 variable "security_group_ids" { type = list(string) }
-variable "multi_az" { type = bool default = false }
-variable "tags" { type = map(string) default = {} }
-
+variable "multi_az" {
+  type    = bool
+  default = false
+}
