44c8ed43176427cdceaff9dbdf831f40f32add8e
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:23:05 +0530

    Update variables.tf

--- modules/security-group/variables.tf
+++ modules/security-group/variables.tf
@@ -1,6 +1,9 @@
 variable "name" { type = string }
 variable "vpc_id" { type = string }
-variable "ingress_rules" { type = list(any) default = [] }
+variable "ingress_rules" {
+  type    = list(any)
+  default = []
+}
 variable "tags" {
   type    = map(string)
   default = {}
