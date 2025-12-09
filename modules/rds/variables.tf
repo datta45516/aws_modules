76524d8fd0a32096140109fb4a146300c143fedb
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:03:24 +0530

    Update and rename variable.tf to variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -5,7 +5,10 @@ variable "engine_version" { default = "16.0" }
 variable "instance_class" { default = "db.t3.micro" }
 variable "allocated_storage" { default = 20 }
 variable "username" { type = string }
-variable "password" { type = string sensitive = true }
+variable "password" {
+type = string
+sensitive = true
+}
 variable "subnet_ids" { type = list(string) }
 variable "security_group_ids" { type = list(string) }
 variable "multi_az" { type = bool default = false }
