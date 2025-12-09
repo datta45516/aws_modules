a6c890ad3d09d2b1c880c28e79a4d220ae556c8c
datta45516 <sdattatray516@gmail.com>
2025-12-09 16:02:10 +0530

    Update and rename variable.tf to variables.tf

--- modules/s3_bucket/variable.tf
+++
@@ -1,6 +0,0 @@
-
-variable "region" { type = string default = "us-east-1" }
-variable "bucket_name" { type = string }
-variable "versioning_enabled" { type = bool default = true }
-variable "enable_lifecycle" { type = bool default = true }
-variable "tags" { type = map(string) default = {} }
