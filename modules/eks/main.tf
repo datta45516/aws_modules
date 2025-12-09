0fbf2509b2a12de63632f5a1d5008baad5750484
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:20:29 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -10,15 +10,17 @@ provider "aws" { region = var.region }
 
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
-  assume_role_policy = jsonencode({
+   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "eks.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
   })
-    }]
+}
   })
 }
 
