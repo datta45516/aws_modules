7e3e92d3d15f410f8988ccf1a1f31aeae201a318
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:21:30 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,6 +1,9 @@
 variable "name" { type = string }
 variable "username" { type = string }
-variable "password" { type = string sensitive = true }
+variable "password" {
+  type      = string
+  sensitive = true
+}
 variable "subnet_ids" { type = list(string) }
 variable "security_group_ids" { type = list(string) }
 variable "multi_az" {
