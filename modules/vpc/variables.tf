3f41cf5d6621d50a8d93a43c73d68b9fb4f6ff96
datta45516 <sdattatray516@gmail.com>
2025-12-09 16:25:12 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,9 +1,10 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
 variable "name" { type = string }
 variable "cidr_block" { type = string }
 variable "azs" { type = list(string) }
 variable "public_subnets" { type = list(string) }
 variable "private_subnets" { type = list(string) }
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
