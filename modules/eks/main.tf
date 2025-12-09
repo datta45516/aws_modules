9c95d2a7cc4fc1bf70ead70a9cddb535442e606d
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:29:41 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,26 +1,30 @@
 terraform {
-required_providers {
-aws = {
-source  = "hashicorp/aws"
-version = "~> 5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = var.region
 }
-}
-provider "aws" { region = var.region }
 
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
-   assume_role_policy = jsonencode({
+
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect    = "Allow"
-        Principal = { Service = "eks.amazonaws.com" }
-        Action    = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
     ]
-  })
-}
   })
 }
 
@@ -29,6 +33,7 @@ resource "aws_iam_role_policy_attachment" "cluster" {
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
     "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   ])
+
   role       = aws_iam_role.cluster.name
   policy_arn = each.value
 }
@@ -37,8 +42,11 @@ resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
   version  = var.kubernetes_version
+
   vpc_config {
     subnet_ids = var.subnet_ids
   }
+
   tags = var.tags
 }
+
