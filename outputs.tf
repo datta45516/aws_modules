f0369c91b87634d0e9be6ee34ca69faf074e187d
datta45516 <sdattatray516@gmail.com>
2025-12-09 14:16:15 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-  description = "ID of the VPC"
+  description = "ID of the created VPC"
   value       = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  description = "Public subnet IDs"
+  description = "List of public subnet IDs"
   value       = module.vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-  description = "Private subnet IDs"
+  description = "List of private subnet IDs"
   value       = module.vpc.private_subnet_ids
 }
 
@@ -24,21 +24,21 @@ output "eks_cluster_name" {
 }
 
 output "alb_dns_name" {
-  description = "ALB DNS name"
+  description = "Application Load Balancer DNS name"
   value       = module.alb.alb_dns_name
 }
 
 output "lambda_function_arn" {
-  description = "Hello Lambda ARN"
+  description = "Hello Lambda function ARN"
   value       = module.lambda.function_arn
 }
 
 output "rds_endpoint" {
-  description = "RDS database endpoint"
+  description = "PostgreSQL RDS endpoint"
   value       = module.rds.endpoint
 }
 
 output "cloudfront_domain" {
-  description = "CloudFront distribution domain"
- value       = module.cloudfront.domain_name
+  description = "CloudFront distribution domain name"
+  value       = module.cloudfront.domain_name
 }
