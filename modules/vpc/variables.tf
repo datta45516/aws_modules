882994f94e1ba3baba022e5e959b28ecf56f75fa
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:04:41 +0530

    Update and rename variable.tf to variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,4 +1,7 @@
-variable "region" { type = string default = "us-east-1" }
+variable "region" {
+type = string
+default = "us-east-1"
+}
 variable "name" { type = string }
 variable "cidr_block" { type = string }
 variable "azs" { type = list(string) }
