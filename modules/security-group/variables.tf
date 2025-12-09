4b2c14b0898fab915c7dc643f2c1a0c6c1491a79
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:14:37 +0530

    Create variables.tf

---
+++ modules/security-group/variables.tf
@@ -0,0 +1,7 @@
+variable "name" { type = string }
+variable "vpc_id" { type = string }
+variable "ingress_rules" { type = list(any) default = [] }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
