dce23e86e0cdf71fa4b610de5c653e6f00c8d0f0
datta45516 <sdattatray516@gmail.com>
2025-12-09 14:54:32 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,8 +1,10 @@
-
 variable "region" { default = "us-east-1" }
 variable "name" { type = string }
 variable "subnet_ids" { type = list(string) }
 variable "security_group_ids" { type = list(string) }
 variable "vpc_id" { type = string }
 variable "certificate_arn" { type = string }
-variable "tags" { type = map(string) default = {} }
+variable "tags" {
+type = map(string)
+default = {}
+}
