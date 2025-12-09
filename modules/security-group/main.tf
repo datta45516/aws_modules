77c5c1c4b61095d959202d7dadea139b2ac1c976
datta45516 <sdattatray516@gmail.com>
2025-12-09 15:16:20 +0530

    Update main.tf

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -1,5 +1,11 @@
-
-terraform { required_providers { aws = { source = "hashicorp/aws" version = "~> 5.0" } } }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 provider "aws" { region = var.region }
 
 resource "aws_security_group" "this" {
